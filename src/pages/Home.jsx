2462da8e8117d486d4990d82aed9a3a7e19e4320
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-10-12 20:40:35 +0600

    footer design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Collection from './Home/Home/Collection';
 import DreamProduct from './Home/Home/DreamProduct';
 import Features from './Home/Home/Features';
 import Gadget from './Home/Home/Gadget';
-import Gallery from './Home/Home/Gallery';
 import Offered from './Home/Home/Offered';
 import Reviews from './Home/Home/Reviews';
 import ViewProduct from "./Home/Home/ViewProduct";
@@ -17,7 +16,6 @@ const Home = () => {
                         <Benner></Benner>
                         <Marqueecard></Marqueecard> 
                        <CategoryItem></CategoryItem>
-                       <Gallery></Gallery>
                        <Features></Features>
                        <Collection></Collection>
                        <ViewProduct></ViewProduct>
